15dc71cd6fc64019278730cf130f77f6dddfde6f
Olivier Bourgault <bourgault.olivier8501@gmail.com>
2025-04-01 20:17:10 -0400

    Ajout de 'MultiDev' et 'Phoneword'

---
+++ Personnel/MultiDev/MultiDev/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiDev
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Personnel/MultiDev/MultiDev/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiDev
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
