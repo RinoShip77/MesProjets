73e7623e7d7b5fee0f58490aaa6d38e523316289
Olivier Bourgault <bourgault.olivier8501@gmail.com>
2025-04-08 22:28:41 -0400

    Final update
    Add comments, loops, etc.

--- Personnel/NoteMaker/notepad.py
+++ Personnel/NoteMaker/notepad.py
@@ -5,6 +5,7 @@ from PIL import Image
 from tkinter import filedialog
 import speech_recognition
 import pyttsx3
+import language_tool_python
 
 # Set some constants
 customtkinter.set_default_color_theme("green")
@@ -12,12 +13,14 @@ openIcon = customtkinter.CTkImage(light_image=Image.open('img/open.png'), dark_i
 saveIcon = customtkinter.CTkImage(light_image=Image.open('img/save.png'), dark_image=Image.open('img/save.png'), size=(20,20))
 speakIcon = customtkinter.CTkImage(light_image=Image.open('img/speak.png'), dark_image=Image.open('img/speak.png'), size=(20,20))
 readIcon = customtkinter.CTkImage(light_image=Image.open('img/read.png'), dark_image=Image.open('img/read.png'), size=(20,20))
+grammarCheckIcon = customtkinter.CTkImage(light_image=Image.open('img/grammarCheck.png'), dark_image=Image.open('img/grammarCheck.png'), size=(20,20))
 
+# UI component as classes
 class Submenu(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.grid_columnconfigure((0, 1), weight=1)
         self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure((0, 1), weight=1)
 
         # Add buttons to frame
         self.openBtn = customtkinter.CTkButton(self, text="Open File", image=openIcon, hover=True, command=OpenFile)
@@ -31,6 +34,9 @@ class Submenu(customtkinter.CTkFrame):
         
         self.readBtn = customtkinter.CTkButton(self, text="Read", image=readIcon, hover=True, command=Read)
         self.readBtn.grid(row=0, column=3, padx=5, pady=15, sticky="nsew")
+        
+        self.grammarCheckBtn = customtkinter.CTkButton(self, text="Grammar check", image=grammarCheckIcon, hover=True, command=GrammarCheck)
+        self.grammarCheckBtn.grid(row=0, column=4, padx=5, pady=15, sticky="nsew")
 
 class Menu(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
@@ -39,8 +45,8 @@ class Menu(customtkinter.CTkFrame):
         self.columnconfigure(2, weight=1)
 
         # Add the submenu and the combobox to the frame
-        self.menu = Submenu(self)
-        self.menu.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+        self.submenu = Submenu(self)
+        self.submenu.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
 
         self.combobox = customtkinter.CTkComboBox(self, values=["system", "dark", "light"], hover=True, command=ChangeMode)
         self.combobox.grid(row=0, column=2, padx=10, pady=20, sticky="e")
@@ -51,14 +57,15 @@ class TabView(customtkinter.CTkTabview):
         super().__init__(master, **kwargs)
         self.grid_columnconfigure(0, weight=1)
 
-        # Create tabs
+        # Create starting tabs
         self.add("New file")
         self.add("+")
 
-        # Add a textbox in the tabs
+        # Set configurations for the first tab
         self.tab("New file").grid_rowconfigure(0, weight=1)
         self.tab("New file").grid_columnconfigure(0, weight=1)
 
+        # Add a textbox in the first tab
         self.textbox = customtkinter.CTkTextbox(self.tab("New file"), corner_radius=5, wrap="word")
         self.textbox.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
@@ -67,7 +74,7 @@ class App(customtkinter.CTk):
         super().__init__()
         self.title("NoteMaker")
         self.geometry("1500x1000")
-        self.minsize(width=700, height=200)
+        self.minsize(width=1000, height=200)
         self.resizable(True, True)
         self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(0, weight=1)
@@ -81,45 +88,10 @@ class App(customtkinter.CTk):
         self.tabView.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
 
 # Add methods to app
-def ChangeMode(choice):
-    customtkinter.set_appearance_mode(choice)
-
-def OpenFile():
-    file = filedialog.askopenfile(mode='r', title="Open file", filetype=[("*. txt")])
-    if file is not None:
-        content = file.read()
-    app.tabView.rename("New file", file.name.split("/")[len(file.name.split("/")) - 1].split(".")[0])
-    app.tabView.set(file.name.split("/")[len(file.name.split("/")) - 1].split(".")[0])
-    app.tabView.textbox.insert("0.0", content)
-        
-def SaveFile():
-    openFile = filedialog.asksaveasfile(mode='w', title="Save file", initialfile=app.tabView.get(), filetype=[('text files', '*.txt')])
-    if openFile is None:
-        return
-    app.tabView.rename("New file", openFile.name.split("/")[len(openFile.name.split("/")) - 1].split(".")[0])
-    app.tabView.set(openFile.name.split("/")[len(openFile.name.split("/")) - 1].split(".")[0])
-    text = app.tabView.textbox.get("0.0","end")
-    openFile.write(text)
-    openFile.close()
-
-def Speak():
-    with speech_recognition.Microphone() as source:
-        recognizer = speech_recognition.Recognizer()
-        # read the audio data from the default microphone
-        audio_data = recognizer.record(source, duration=5)
-        # convert speech to text
-        text = recognizer.recognize_google(audio_data)
-        app.tabView.textbox.insert("0.0", text)
-
-def Read():
-    engine = pyttsx3.init()
-    text = app.tabView.textbox.get("0.0","end")
-    engine.say(text)
-    engine.runAndWait()
-
 def NewTab(title = None):
     if app.tabView.get() == "+":
         if title is None:
+            # Dynamically create a new tab
             title = "New file"
             app.tabView.insert(app.tabView.index("+"), title)
             app.tabView.tab(title).grid_rowconfigure(0, weight=1)
@@ -127,6 +99,44 @@ def NewTab(title = None):
             app.tabView.textbox = customtkinter.CTkTextbox(app.tabView.tab(title), corner_radius=5, wrap="word")
             app.tabView.textbox.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
             app.tabView.set(title)
+
+def SetTabs(currentTab, title):
+    # Dynamically organise the existing tabs
+    app.tabView.rename(currentTab, title)
+    app.tabView.set(title)
+
+def ChangeMode(choice):
+    customtkinter.set_appearance_mode(choice)
+
+def OpenFile():
+    file = filedialog.askopenfile(mode='r', title="Open file", filetype=[("*. txt")])
+    if file is not None:
+        content = file.read()
+    SetTabs(app.tabView.get(), file.name.split("/")[len(file.name.split("/")) - 1].split(".")[0])
+    app.tabView.textbox.insert("0.0", content)
+        
+def SaveFile():
+    openFile = filedialog.asksaveasfile(mode='w', title="Save file", initialfile=app.tabView.get(), filetype=[('text files', '*.txt')])
+    if openFile is None:
+        return
+    SetTabs("New file", openFile.name.split("/")[len(openFile.name.split("/")) - 1].split(".")[0])
+    openFile.write(app.tabView.textbox.get("0.0","end"))
+    openFile.close()
+
+def Speak():
+    with speech_recognition.Microphone() as source:
+        # Convert speech to text from the default microphone
+        app.tabView.textbox.insert("end", speech_recognition.Recognizer().recognize_google(speech_recognition.Recognizer().record(source, duration=5)))
+
+def Read():
+    engine = pyttsx3.init()
+    engine.say(app.tabView.textbox.get("0.0","end"))
+    engine.runAndWait()
+
+def GrammarCheck():
+    with language_tool_python.LanguageToolPublicAPI('en-CA') as checker:
+        if checker.check(app.tabView.textbox.get("0.0","end")) is not None:
+            app.tabView.textbox.insert("0.0", checker.correct(app.tabView.textbox.get("0.0","end")))
 
 customtkinter.set_appearance_mode("system")
 # Create an window instance
