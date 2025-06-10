3b52c8b534169aa691cc7c9a2dba99e54e3c8645
Olivier Bourgault <bourgault.olivier8501@gmail.com>
2025-06-10 17:24:12 -0400

    Update

--- Personnel/MyBuilder/Backend/src/controllers/order.controller.js
+++ Personnel/MyBuilder/Backend/src/controllers/order.controller.js
@@ -4,16 +4,16 @@ require("dotenv").config();
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 exports.create = async function (req, res) {
-  // let product = await stripe.products.create({
-  //   name: req.body.name,
-  //   images: [`https:${req.body.imageURL}`],
-  //   default_price_data: {
-  //     currency: "cad",
-  //     unit_amount: (parseFloat(req.body.price.slice(0, -2)) * 100)
-  //   }
-  // });
+  let product = await stripe.products.create({
+    name: req.body.name,
+    images: [`https:${req.body.imageURL}`],
+    default_price_data: {
+      currency: "cad",
+      unit_amount: (parseFloat(req.body.price.slice(0, -2)) * 100)
+    }
+  });
 
-  // res.json({ priceID: product.default_price });
+  res.json({ priceID: product.default_price });
 };
 
 exports.checkout = async function (req, res) {
