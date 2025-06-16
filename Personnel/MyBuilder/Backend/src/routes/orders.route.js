4300ce16753739d9c4e0a16b3943eeca890a077d
Olivier Bourgault <bourgault.olivier8501@gmail.com>
2025-06-15 21:22:45 -0400

    Update the route

--- Personnel/MyBuilder/Backend/src/routes/orders.route.js
+++ Personnel/MyBuilder/Backend/src/routes/orders.route.js
@@ -6,6 +6,5 @@ const router = express.Router();
 // Routes
 router.post("/checkout", orderController.checkout);
 router.get("/checkout/success", orderController.checkoutSuccess);
-router.get("/checkout/cancel", orderController.checkoutCancel);
 
 module.exports = router;
