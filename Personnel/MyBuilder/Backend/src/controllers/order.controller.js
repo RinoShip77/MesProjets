b8d516eb37ebded7c2383b051bef52ca5821f088
Olivier Bourgault <bourgault.olivier8501@gmail.com>
2025-06-27 09:24:44 -0400

    Add the getAll and the getOne order

--- Personnel/MyBuilder/Backend/src/controllers/order.controller.js
+++ Personnel/MyBuilder/Backend/src/controllers/order.controller.js
@@ -3,10 +3,25 @@ require("dotenv").config();
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 let statusURL = "";
 
-exports.checkout = async function (req, res) {  
+exports.getAll = async function (req, res) {
+  const orders = await stripe.checkout.sessions.list({
+    limit: 100,
+  });
+
+
+  res.json(orders.data);
+};
+
+exports.getOne = async function (req, res) {
+  const order = await stripe.checkout.sessions.retrieve(req.params.id);
+
+  res.json(order);
+};
+
+exports.checkout = async function (req, res) {
   statusURL = req.body.statusURL;
 
-  let checkout = {
+  let data = {
     line_items: [
       {
         price_data: {
@@ -23,17 +38,17 @@ exports.checkout = async function (req, res) {
     mode: "payment",
     success_url: `${req.protocol}://${req.get('host')}/orders/checkout/success?id={CHECKOUT_SESSION_ID}`,
   };
-  
-  if(req.body.user.length !== 0)
-    checkout.customer_email = req.body.user;
-  
-  const checkoutSession = await stripe.checkout.sessions.create(checkout);
-  
-  res.json(checkoutSession.url)
+
+  if (req.body.user.length !== 0)
+    data.customer_email = req.body.user;
+
+  const checkout = await stripe.checkout.sessions.create(data);
+
+  res.json(checkout.url)
 };
 
 exports.checkoutSuccess = async function (req, res) {
-  const checkoutSession = await stripe.checkout.sessions.retrieve(req.query.id);
-  
-  res.redirect(`${statusURL}${req._parsedUrl.pathname.slice(req._parsedUrl.pathname.lastIndexOf("/"))}Checkout.html?name=${checkoutSession.customer_details.name}&order=${checkoutSession.id}&total=${checkoutSession.amount_total}&date=${checkoutSession.created}`);
+  const checkout = await stripe.checkout.sessions.retrieve(req.query.id);
+
+  res.redirect(`${statusURL}${req._parsedUrl.pathname.slice(req._parsedUrl.pathname.lastIndexOf("/"))}Checkout.html?name=${checkout.customer_details.name}&order=${checkout.id}&total=${checkout.amount_total}&date=${checkout.created}`);
 };
--- Personnel/MyBuilder/Backend/src/routes/orders.route.js
+++ Personnel/MyBuilder/Backend/src/routes/orders.route.js
@@ -4,6 +4,8 @@ const orderController = require("../controllers/order.controller");
 const router = express.Router();
 
 // Routes
+router.get("/", orderController.getAll);
+router.get("/:id", orderController.getOne);
 router.post("/checkout", orderController.checkout);
 router.get("/checkout/success", orderController.checkoutSuccess);
 
